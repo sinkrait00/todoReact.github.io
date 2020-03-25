44e97196a12fe741aa35e3c77380d3fd1031ee59
Arseniy Rybalov <mrsinkrait@gmail.com>
2020-03-25 22:58:23 +0600

    Updates

---
+++ precache-manifest.0ddf7bcece8257285c3b6a2b16b26ecf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771a5fe7a4c34ab395150db76cf30c81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1026e80500b31da6e09",
+    "url": "/static/css/main.6b9f208f.chunk.css"
+  },
+  {
+    "revision": "1f68367318aae209e11c",
+    "url": "/static/js/2.7f746860.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.7f746860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1026e80500b31da6e09",
+    "url": "/static/js/main.e37e2625.chunk.js"
+  },
+  {
+    "revision": "f994a34bba0f72505f57",
+    "url": "/static/js/runtime-main.3ad01945.js"
+  }
+]);
